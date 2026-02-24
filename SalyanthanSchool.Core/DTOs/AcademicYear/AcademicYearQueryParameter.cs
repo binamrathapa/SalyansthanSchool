76aa40f78955be53b65bbbaa21fde35c9554be9a
yujanThulung <yujanrai07@gmail.com>
2026-02-24 21:19:19 +0545

    refact/academic-year-api refactor

---
+++ SalyanthanSchool.Core/DTOs/AcademicYear/AcademicYearQueryParameter.cs
@@ -0,0 +1,9 @@
+﻿using SalyanthanSchool.Core.Common;
+
+namespace SalyanthanSchool.Core.DTOs.AcademicYear
+{
+    public class AcademicYearQueryParameter : QueryParameter
+    {
+        public bool? IsActive { get; set; }
+    }
+}
